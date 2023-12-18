dd3983bb1aaea7dc05528c6fd7f40af3af527578
Kaykanloo <kaykanloo@gmail.com>
2023-12-18 10:53:47 -0500

    Adding sample components

---
+++ components/TextBox.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineProps({
+  label: { type: String, required: true },
+  modelValue: { type: String }
+})
+defineEmits(['update:modelValue'])
+</script>
+
+<template>
+  <div class="container">
+    <label :for="label">{{ label }}:</label>
+    <input :id="label" :value="modelValue" @input="$emit('update:modelValue', $event.target?.value)" />
+  </div>
+</template>
+
+<style scoped>
+.container {
+  border: 1px dashed;
+  margin: 4px;
+}
+</style>
+
+
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const name = ref('World')
+const msg = computed(() => `Hello ${capitalizeStr(name.value)}!`)
+</script>
+
+<template>
+  <TextBox label="Your Name" v-model="name" />
+  <MessageBox :message="msg"></MessageBox>
+</template>
+
+<style scoped></style>
