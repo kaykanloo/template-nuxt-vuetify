81deffd062fb4d42639c9eeaa4ba8468abda5d2f
Kaykanloo <kaykanloo@gmail.com>
2024-01-03 11:32:48 -0500

    Adding Vuetify

--- app.vue
+++ app.vue
@@ -1,3 +1,26 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const links = ref([
+  {
+    text: 'Dashboard',
+    href: '/',
+  },
+  {
+    text: 'Storybook',
+    href: '/storybook',
+  },
+])
+</script>
+
 <template>
-  <NuxtPage />
+  <NuxtLayout>
+    <v-app>
+      <AppBar :links="links" />
+      <v-main>
+        <v-container>
+          <NuxtPage />
+        </v-container>
+      </v-main>
+    </v-app>
+  </NuxtLayout>
 </template>
---
+++ components/AppBar.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import type { PropType } from 'vue';
+type Link = {
+  text: string;
+  href: string;
+};
+defineProps({
+  links: { type: Array as PropType<Link[]>, required: true },
+})
+</script>
+
+<template>
+  <v-app-bar flat>
+    <v-container class="mx-auto d-flex align-center justify-center">
+      <v-avatar class="me-4 " color="grey-darken-1" size="32"></v-avatar>
+
+      <v-btn v-for="link in links" :key="link.text" :text="link.text" :href="link.href" variant="text"></v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-responsive max-width="320">
+        <v-text-field density="compact" flat hide-details label="Search" rounded="lg" single-line
+          variant="solo-filled"></v-text-field>
+      </v-responsive>
+    </v-container>
+  </v-app-bar>
+</template>
+
+<style scoped></style>
+
+
