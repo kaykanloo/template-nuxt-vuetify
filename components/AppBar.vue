3de04de0a624622d6944604b97fa4a288739dcf9
Kaykanloo <kaykanloo@gmail.com>
2024-01-04 14:04:46 -0500

    Adding Light/Dark theme toggle

--- components/AppBar.vue
+++ components/AppBar.vue
@@ -7,12 +7,18 @@ type Link = {
 defineProps({
   links: { type: Array as PropType<Link[]>, required: true },
 })
+
+import { useTheme } from 'vuetify'
+const theme = useTheme()
+const toggleTheme = () => {
+  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+}
 </script>
 
 <template>
   <v-app-bar flat>
     <v-container class="mx-auto d-flex align-center justify-center">
-      <v-avatar class="me-4 " color="grey-darken-1" size="32"></v-avatar>
+      <v-icon icon="mdi-alpha-t-box-outline" size="36" />
 
       <v-btn v-for="link in links" :key="link.text" :text="link.text" :href="link.href" variant="text"></v-btn>
 
@@ -22,6 +28,7 @@ defineProps({
         <v-text-field density="compact" flat hide-details label="Search" rounded="lg" single-line
           variant="solo-filled"></v-text-field>
       </v-responsive>
+      <v-btn icon="mdi-theme-light-dark" @click="toggleTheme"></v-btn>
     </v-container>
   </v-app-bar>
 </template>
